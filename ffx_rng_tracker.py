63f16a9af0a0badd394cdabf2c96174b24cec311
Grayfox96 <77098882+Grayfox96@users.noreply.github.com>
2021-07-16 12:02:35 +0200

    added event_ inner functions
    added event_ inner functions that show more meaningful messages and added a specific highlight for errors

--- ffx_rng_tracker.py
+++ ffx_rng_tracker.py
@@ -156,15 +156,11 @@ def patch_monsters_array_for_hd(monsters_array):
 	# monsters_array['valaha'] = patch_abilities(monsters_array['valaha'], (67, 67, 67, 30, 30, 127, 127))
 	# monsters_array['yowie'] = patch_abilities(monsters_array['yowie'], (126, 126, 126, 38, 38, 30, 42))
 
-
 	return monsters_array
-
 
 monsters_array = patch_monsters_array_for_hd(monsters_array)
 
-
 def make_predictions(steal_drop_seed, common_rare_seed, equipment_seed, abilities_seed, abilities_array, items_array, monsters_array):
-
 
 	def get_equipment_types():
 		rng_equipment = get_rng_generator(equipment_seed)
@@ -183,9 +179,7 @@ def make_predictions(steal_drop_seed, common_rare_seed, equipment_seed, abilitie
 
 		return equipment_types
 
-
 	def get_good_early_game_drops(abilities_to_test, characters_to_test, number_of_kills_range, number_of_steals_range):
-
 
 		def get_good_equipment(enemy, characters_enabled_string, killer_index, abilities_to_test):
 			prize_struct = get_prize_struct(enemy, monsters_array)
@@ -201,7 +195,6 @@ def make_predictions(steal_drop_seed, common_rare_seed, equipment_seed, abilitie
 							return False
 
 			return False
-
 
 		good_early_game_drops = []
 
@@ -242,7 +235,6 @@ def make_predictions(steal_drop_seed, common_rare_seed, equipment_seed, abilitie
 							'number_of_steals': number_of_steals, 'equipment': equipment})
 
 		return good_early_game_drops
-
 
 	for equipment_n, equipment_type in get_equipment_types().items():
 		print(f'Equipment {"#" + str(equipment_n):>3}: {equipment_type}')
@@ -294,6 +286,104 @@ def parse_notes(abilities_array, items_array, monsters_array, data_text):
 			i += 1
 			yield i
 
+	def event_steal(*params):
+		if len(params) < 2:
+			return 'Usage: steal [enemy_name] [successful steals]'
+		elif len(params) >= 2:
+			(monster, successful_steals) = params[:2]
+
+		successful_steals = int(successful_steals)
+		prize_struct = get_prize_struct(monster, monsters_array)
+
+		if prize_struct == False:
+			next(rng_steal_drop)
+			next(rng_common_rare)
+			return f'No monster named "{monster}"'
+		else:
+			monster = monster.replace('_', ' ')
+			monster = ' '.join([word[0].upper() + word[1:] for word in monster.split(' ')])
+			return f'Steal from {monster}: ' + get_stolen_item(prize_struct, items_array, successful_steals, rng_steal_drop, rng_common_rare)
+
+	def event_kill(*params):
+
+		if len(params) < 2:
+			return 'Usage: kill [enemy_name] [killer]'
+		elif len(params) >= 2:
+			(monster, killer) = params[:2]
+
+		killer_index = (	0 if killer.lower() == 'tidus' else 
+							1 if killer.lower() == 'yuna' else 
+							2 if killer.lower() == 'auron' else 
+							3 if killer.lower() == 'kimahri' else 
+							4 if killer.lower() == 'wakka' else 
+							5 if killer.lower() == 'lulu' else 
+							6 if killer.lower() == 'rikku' else 7
+							)
+
+		prize_struct = get_prize_struct(monster, monsters_array)
+
+		if prize_struct == False:
+			return f'No monster named "{monster}"'
+
+		else:
+			item1, item2, equipment = get_spoils(prize_struct, abilities_array, items_array,
+				characters_enabled_string, killer_index, rng_steal_drop, rng_common_rare, rng_equipment, rng_abilities)
+
+			monster = monster.replace('_', ' ')
+			monster = ' '.join([word[0].upper() + word[1:] for word in monster.split(' ')])
+
+			result = f'{monster} drops: '
+
+			if item1: result += f'{item1}'
+
+			if item2: result += f', {item2}'
+
+			if equipment:
+				guaranteed_equipment = ' (guaranteed)' if equipment["guaranteed"] else '' 
+				result += (	f', Equipment #{next(equipment_counter)}{guaranteed_equipment}: {equipment["type"]} for {equipment["character"]} '
+					f'{[ability for slot, ability in equipment["abilities"].items()]} [{equipment["sell_gil_value"]} gil]')
+
+			# if all 3 are False
+			if any((item1, item2, equipment)) == False: result += 'No drops'
+
+			return result
+
+	def event_death(*params):
+		for i in range(3):
+			next(rng_steal_drop)
+
+		return 'Character death'
+
+	def event_roll(*params):
+		if len(params) < 2:
+			return 'Usage: waste/advance/roll [rng#] [amount]'
+		if len(params) >= 2:
+			(rng, number_of_times) = params[:2]
+
+		for i in range(int(number_of_times)):
+			if rng == 'rng10': next(rng_steal_drop)
+			elif rng == 'rng11': next(rng_common_rare)
+			elif rng == 'rng12': next(rng_equipment)
+			elif rng == 'rng13': next(rng_abilities)
+			else: return f'Can\'t advance {rng}'
+
+		return f'Advanced {rng} {number_of_times} times'
+
+	# aliases
+	event_advance = event_roll
+	event_waste = event_roll
+
+	def event_party(*params):
+		nonlocal characters_enabled_string
+
+		valid_party_formations = ('ta', 't', 'tw', 'tywl', 'tykwl', 'tyakwl', 'tyakwlr', 'takwlr', 'ya', 'twr', 'tyawlr')
+
+		if characters_enabled_string in valid_party_formations:
+			characters_enabled_string = params[0]
+			return f'Party changed to {characters_enabled_string}'
+		else:
+			return 'Invalid party formation'
+
 	equipment_counter = get_equipment_counter()
 	rng_steal_drop = get_rng_generator(current_steal_drop_seed)
 	rng_common_rare = get_rng_generator(current_common_rare_seed)
@@ -306,7 +396,7 @@ def parse_notes(abilities_array, items_array, monsters_array, data_text):
 	for line in notes_lines_array:
 		if line != '':
 			# fixes double spaces
-			line = " ".join(line.split())
+			line = ' '.join(line.split())
 
 			if line[:3] == '///':
 				data = ''
@@ -318,84 +408,20 @@ def parse_notes(abilities_array, items_array, monsters_array, data_text):
 
 			try:
 				line = line.lower()
-				event, params = [split for split in line.split(' ', 1)]
+				event, *params = [split for split in line.split(' ')]
 
-				if event == 'steal':
-					monster, successful_steals = [split for split in params.split(' ', 1)]
-					successful_steals = int(successful_steals)
-					prize_struct = get_prize_struct(monster, monsters_array)
-					if prize_struct == False:
-						data += f'No monster named "{monster}"'
-						next(rng_steal_drop)
-						next(rng_common_rare)
-					else:
-						monster = monster.replace('_', ' ')
-						monster = ' '.join([word[0].upper() + word[1:] for word in monster.split(' ')])
-						data += f'Steal from {monster}: ' + get_stolen_item(prize_struct, items_array, successful_steals, rng_steal_drop, rng_common_rare)
+				try:
+					data += locals()['event_' + event](*params)
+				except KeyError as error:
+					# if event doesnt exists
+					data += f'No event called {event}'
 
-				elif event == 'kill':
-					monster, killer = [split for split in params.split(' ', 2)]
-
-					killer_index = (	0 if killer.lower() == 'tidus' else 
-										1 if killer.lower() == 'yuna' else 
-										2 if killer.lower() == 'auron' else 
-										3 if killer.lower() == 'kimahri' else 
-										4 if killer.lower() == 'wakka' else 
-										5 if killer.lower() == 'lulu' else 
-										6 if killer.lower() == 'rikku' else 7
-										)
-
-					prize_struct = get_prize_struct(monster, monsters_array)
-
-					if prize_struct == False:
-						data += f'No monster named "{monster}"'
-
-					else:
-						item1, item2, equipment = get_spoils(prize_struct, abilities_array, items_array,
-							characters_enabled_string, killer_index, rng_steal_drop, rng_common_rare, rng_equipment, rng_abilities)
-
-						monster = monster.replace('_', ' ')
-						monster = ' '.join([word[0].upper() + word[1:] for word in monster.split(' ')])
-
-						data += f'{monster} drops: '
-
-						if item1: data += f'{item1}'
-
-						if item2: data += f', {item2}'
-
-						if equipment:
-							guaranteed_equipment = ' (guaranteed)' if equipment["guaranteed"] else '' 
-							data += (	f', Equipment #{next(equipment_counter)}{guaranteed_equipment}: {equipment["type"]} for {equipment["character"]} '
-										f'{[ability for slot, ability in equipment["abilities"].items()]} [{equipment["sell_gil_value"]} gil]')
-
-						# if all 3 are False
-						if any((item1, item2, equipment)) == False: data += 'No drops'
-
-				elif event == 'death':
-					data += 'Character death'
-					next(rng_steal_drop)
-					next(rng_steal_drop)
-					next(rng_steal_drop)
-
-				elif event == 'waste' or event == 'advance' or event == 'roll':
-					rng, number_of_times = [split for split in params.split(' ', 1)]
-					for i in range(int(number_of_times)):
-						if rng == 'rng10': next(rng_steal_drop)
-						elif rng == 'rng11': next(rng_common_rare)
-						elif rng == 'rng12': next(rng_equipment)
-						elif rng == 'rng13': next(rng_abilities)
-					data += f'Advanced {rng} {number_of_times} times'
-
-				elif event == 'party':
-					characters_enabled_string = params.split(' ', 1)[0]
-					data += 'Party changed'
-
-				else: data += 'Invalid formatting'
 			except ValueError as error:
 				data += 'Invalid formatting'
 
 		data += '\n'
 
+	# remove the last newline
 	data = data[:-2]
 	data_text.config(state='normal')
 	data_text.delete(1.0,'end')
@@ -405,12 +431,14 @@ def parse_notes(abilities_array, items_array, monsters_array, data_text):
 	highlight_pattern(data_text, 'No Encounters', 'no_encounters')
 	highlight_pattern(data_text, '^#(.+?)?$', 'comment', regexp=True)
 	highlight_pattern(data_text, '^Advanced rng.+$', 'rng_rolls', regexp=True)
-	
+	for error_message in ('No event called', 'Usage:', 'No monster named'):
+		highlight_pattern(data_text, f'^{error_message}.+$', 'error', regexp=True)
 
 	data_text.config(state='disabled')
 
 	# on_notes_scroll(notes_scroll_position)
 	data_text.yview('moveto', data_text_scroll_position)
+
 
 # GUI
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -436,7 +464,8 @@ data_text.pack(expand=True, fill='both', side='right')
 data_text.tag_configure('equipment', foreground='#0000ff')
 data_text.tag_configure('no_encounters', foreground='#00ff00')
 data_text.tag_configure('comment', foreground='#888888')
-data_text.tag_configure('rng_rolls', background='#ff0000')
+data_text.tag_configure('rng_rolls', foreground='#ff0000')
+data_text.tag_configure('error', background='#ff0000')
 
 
 notes = tk.Text(root, font=texts_font, width=43)
