9d0f83616809d51568cbb918a47f9ffe3d412e04
Grayfox96 <77098882+Grayfox96@users.noreply.github.com>
2021-10-25 23:34:00 +0200

    Update monster_data_viewer.py

--- monster_data_viewer.py
+++ monster_data_viewer.py
@@ -1,5 +1,7 @@
+import sys
 import tkinter as tk
 
+from ffx_rng_tracker.data.file_functions import get_resource_path
 from ffx_rng_tracker.logger import (log_exceptions, log_tkinter_error,
                                     setup_logger)
 from ffx_rng_tracker.ui_tkinter.monster_data_viewer import MonsterDataViewer
@@ -12,6 +14,16 @@ def main():
     root.protocol('WM_DELETE_WINDOW', lambda: root.quit())
     root.title('ffx_monster_data_viewer')
     root.geometry('1280x800')
+
+    if '-notheme' not in sys.argv:
+        theme_path = get_resource_path(
+            'ffx_rng_tracker/ui_tkinter/azure_theme/azure.tcl')
+        root.tk.call('source', theme_path)
+        if '-darkmode' in sys.argv:
+            root.tk.call('set_theme', 'dark')
+        else:
+            root.tk.call('set_theme', 'light')
+
     # redirects errors to another function
     root.report_callback_exception = log_tkinter_error
     ui = MonsterDataViewer(root)
